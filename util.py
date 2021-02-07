869d29e02f162a8fbb2404cbf3c3a3f741f471c7
Tianyu Lu <tianyu.lu@mail.utoronto.ca>
2021-02-07 11:03:50 -0500

    Update util.py

--- util.py
+++ util.py
@@ -12,6 +12,16 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torch.utils.data.sampler import SubsetRandomSampler
 from sklearn.model_selection import train_test_split, ShuffleSplit
 from scipy import stats
+from typing import Union, List, Tuple, Sequence, Dict, Any, Optional, Collection
+from copy import copy
+from pathlib import Path
+import pickle as pkl
+import logging
+import random
+import lmdb
+from scipy.spatial.distance import pdist, squareform
+
+
 plt.rcParams['figure.dpi'] = 300
 
 
@@ -22,6 +32,56 @@ IDX_AA = {i:AA[i].upper() for i in range(len(AA))}
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class LMDBDataset(Dataset):
+    """Creates a dataset from an lmdb file.
+    Args:
+        data_file (Union[str, Path]): Path to lmdb file.
+        in_memory (bool, optional): Whether to load the full dataset into memory.
+            Default: False.
+    """
+
+    def __init__(self,
+                 data_file: Union[str, Path],
+                 in_memory: bool = False):
+
+        data_file = Path(data_file)
+        if not data_file.exists():
+            raise FileNotFoundError(data_file)
+
+        env = lmdb.open(str(data_file), max_readers=1, readonly=True,
+                        lock=False, readahead=False, meminit=False)
+
+        with env.begin(write=False) as txn:
+            num_examples = pkl.loads(txn.get(b'num_examples'))
+
+        if in_memory:
+            cache = [None] * num_examples
+            self._cache = cache
+
+        self._env = env
+        self._in_memory = in_memory
+        self._num_examples = num_examples
+
+    def __len__(self) -> int:
+        return self._num_examples
+
+    def __getitem__(self, index: int):
+        if not 0 <= index < self._num_examples:
+            raise IndexError(index)
+
+        if self._in_memory and self._cache[index] is not None:
+            item = self._cache[index]
+        else:
+            with self._env.begin(write=False) as txn:
+                item = pkl.loads(txn.get(str(index).encode()))
+                if 'id' not in item:
+                    item['id'] = str(index)
+                if self._in_memory:
+                    self._cache[index] = item
+        return item
+
 
 
 def one_hot_encode_aa(aa_str, pad=None):
